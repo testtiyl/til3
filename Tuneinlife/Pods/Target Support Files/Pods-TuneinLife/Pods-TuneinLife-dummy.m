fde3f68e7370225f679921557ccffd56d7ab026f
hkp <hitesh.kumar@crownit.in>
2017-01-02 21:14:09 +0530

    pods install

---
+++ Tuneinlife/Pods/Target Support Files/Pods-TuneinLife/Pods-TuneinLife-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TuneinLife : NSObject
+@end
+@implementation PodsDummy_Pods_TuneinLife
+@end
---
+++ Tuneinlife/Pods/Target Support Files/Pods-TuneinLifeTests/Pods-TuneinLifeTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TuneinLifeTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TuneinLifeTestsVersionString[];
+
