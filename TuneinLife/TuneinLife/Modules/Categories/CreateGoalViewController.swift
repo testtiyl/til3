0e31159a51a94328c0893cab22859274a34c51aa
Kanchan Yadav <kanchan.yadav@flyrobe.com>
2017-02-02 20:46:46 +0530

    create goal in progress.

---
+++ TuneinLife/TuneinLife/Modules/Categories/CreateGoalViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateGoalViewController.swift
+//  TuneinLife
+//
+//  Created by Kanchan on 01/02/17.
+//  Copyright © 2017 Shyatoria. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TuneinLife/TuneinLife/Modules/Categories/CustomSquareView.swift
+++ TuneinLife/TuneinLife/Modules/Categories/CustomSquareView.swift
@@ -23,25 +23,43 @@ class CustomSquareView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        if shadowLayer == nil {
-            shadowLayer = CAShapeLayer()
-            shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 12).cgPath
-            shadowLayer.fillColor = UIColor.white.cgColor
-            
-            shadowLayer.shadowColor = UIColor.darkGray.cgColor
-            shadowLayer.shadowPath = shadowLayer.path
-            shadowLayer.shadowOffset = CGSize(width: 2.0, height: 2.0)
-            shadowLayer.shadowOpacity = 0.8
-            shadowLayer.shadowRadius = 2
-            layer.insertSublayer(shadowLayer, at: 0)
-            //layer.insertSublayer(shadowLayer, below: nil) // also works
-        }
-        self.layer.borderWidth = 0.5
-        self.layer.borderColor = UIColor.darkGray.cgColor
-        self.layer.cornerRadius = 12
-        self.layer.opacity = 1.0
-        
+//        if shadowLayer == nil {
+//            shadowLayer = CAShapeLayer()
+//            shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 12).cgPath
+//            shadowLayer.fillColor = UIColor.white.cgColor
+//            
+//            shadowLayer.shadowColor = UIColor.darkGray.cgColor
+//            shadowLayer.shadowPath = shadowLayer.path
+//            shadowLayer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+//            shadowLayer.shadowOpacity = 0.8
+//            shadowLayer.shadowRadius = 2
+//            layer.insertSublayer(shadowLayer, at: 0)
+//            //layer.insertSublayer(shadowLayer, below: nil) // also works
+//        }
+//        self.layer.shadowColor = UIColor.darkGray.cgColor
+//        self.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+//        self.layer.shadowOpacity = 0.8
+//        self.layer.borderWidth = 0.5
+//        self.layer.borderColor = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+//        self.layer.cornerRadius = 12
+//        self.layer.opacity = 1.0
 
+//        let radius: CGFloat = self.frame.width / 2.0 //change it to .height if you need spread for height
+//        let shadowPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 2.1 * radius, height: self.frame.height))
+//        //Change 2.1 to amount of spread you need and for height replace the code for height
+//        
+//        self.layer.cornerRadius = 2
+//        self.layer.shadowColor = UIColor.black.cgColor
+//        self.layer.shadowOffset = CGSize(width: 0.5, height: 0.4)  //Here you control x and y
+//        self.layer.shadowOpacity = 0.5
+//        self.layer.shadowRadius = 5.0 //Here your control your blur
+        self.layer.masksToBounds =  false
+//        self.layer.shadowPath = shadowPath.cgPath
+//        
+        self.layer.borderWidth  = 0.5
+        self.layer.borderColor = UIColor(red: 170, green: 177, blue: 204, alpha: 0.6).cgColor
+        self.layer.cornerRadius = 10
+        
     }
     
 }
